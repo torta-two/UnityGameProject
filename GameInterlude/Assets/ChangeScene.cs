0030fb0485dc15d3f2967e455c09c9ad16609aab
TortA <32102435+torta-two@users.noreply.github.com>
2019-02-02 23:41:55 +0800

    0202

---
+++ GameInterlude/Assets/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int loadSceneBuildIndex = 0;
+
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnLoadScene);
+    }
+
+    private void OnLoadScene()
+    {
+        Interlude.Instance.LoadScene(loadSceneBuildIndex);
+    }
+}
---
+++ GameInterlude/Assets/Interlude.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Interlude
+{
+    private static Interlude _instance;
+    public static Interlude Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new Interlude();
+            return _instance;
+        }
+    }
+
+    private Canvas canvas;
+    private CanvasScaler canvasScaler;
+    private GameObject interludePanel;
+
+    private Interlude()
+    {
+        canvas = new GameObject("Canvas", typeof(Canvas), typeof(CanvasScaler)).GetComponent<Canvas>();        
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvas.sortingOrder = 9999;
+
+        canvasScaler = canvas.GetComponent<CanvasScaler>();
+        canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+
+        interludePanel = Object.Instantiate(Resources.Load("interludePanel"), canvas.transform) as GameObject;
+        interludePanel.transform.SetAsLastSibling();
+        interludePanel.SetActive(false);
+
+        Object.DontDestroyOnLoad(interludePanel);
+        Object.DontDestroyOnLoad(canvas.gameObject);
+    }
+
+    public void LoadScene(int sceneBuildIndex)
+    {
+        interludePanel.SetActive(true);
+        interludePanel.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+
+        //EventHandler temp = BlackSite;
+        //if (temp != null)
+        //{
+        //    temp();
+        //}
+    }
+
+    
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    
+
+    //private void BlackSite(int sceneBuildIndex)
+    //{
+    //    for (int i = 0; i < 5; i++)
+    //    {
+    //        Vector4 tempColor = interludePanel.GetComponent<Image>().color;
+    //        tempColor -= new Vector4(0.2f, 0.2f, 0.2f, -0.2f);
+    //        interludePanel.GetComponent<Image>().color = tempColor;
+
+    //        yield return new WaitForSeconds(0.2f);
+    //    }
+
+    //    SceneManager.LoadScene(sceneBuildIndex);
+    //}
+}
